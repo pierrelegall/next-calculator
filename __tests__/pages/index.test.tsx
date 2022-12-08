699338a605ab909f60a2e612764ec9358fdc9a13
Pierre Le Gall <pierre@legall.im>
2022-12-08 17:06:53 +0100

    Remove boilerplate

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -7,7 +7,7 @@ describe("Home", () => {
     render(<Home />)
 
     const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i
+      name: /Next Calculator/i
     })
 
     expect(heading).toBeInTheDocument()
